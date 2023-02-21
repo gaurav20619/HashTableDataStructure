bdfb956b34b2ab7cbe2d68f2c95dc0b74bc07bb1
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-02-21 07:22:18 +0530

    Add: program to remove a word.

--- src/HashMain.java
+++ src/HashMain.java
@@ -5,11 +5,12 @@ public class HashMain {
 
         System.out.println("---Word Frequency Calculator Program---");
         FrequencyCalculator mapString = new FrequencyCalculator();
-        System.out.println("Input Sentence : " + "Paranoids are not\n" +
-                                                "paranoid because they are paranoid but\n" +
-                                                "because they keep putting themselves\n" +
-                                                "deliberately into paranoid avoidable\n" +
-                                                "situations");
+
+        System.out.println("Input Paragraph : " + "Paranoids are not\n" +
+                                                 "paranoid because they are paranoid but\n" +
+                                                 "because they keep putting themselves\n" +
+                                                 "deliberately into paranoid avoidable\n" +
+                                                 "situations");
         System.out.println();
 
         mapString.resultCalculate("Paranoids are not" +
@@ -18,6 +19,9 @@ public class HashMain {
                                         "deliberately into paranoid avoidable" +
                                         "situations");
 
+        System.out.println();
+
+        mapString.removeWord("Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations", "avoidable");
     }
 }
 
@@ -38,6 +42,27 @@ class FrequencyCalculator {
             }
         }
 
+        System.out.println(value);
+    }
+
+    public void removeWord(String input, String delete) {
+
+        Hashtable<String, Integer> value = new Hashtable<>();
+        String[] words = input.split(" ");
+
+
+        for (String word : words) {
+
+            Integer val = value.get(word);
+            if (val == null) {
+                value.put(word, 1);
+            } else {
+                value.put(word, val + 1);
+            }
+        }
+        System.out.println(value);
+        System.out.println("After Delete");
+        value.remove(delete);
         System.out.println(value);
     }
 }
